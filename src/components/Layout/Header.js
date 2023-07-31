12c6d68ea8ac1ab12059a1384df50e9a80fb38c9
Sun-007 <138206213+Umesh-WW@users.noreply.github.com>
2023-07-31 18:28:29 +0530

    compated navbar header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -29,37 +29,40 @@ export default function Navbar() {
                 onClick={toggleMobileMenu}
                 className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 focus:outline-none focus:ring-inset"
               >
-                {isMobileMenuOpen ? (
-                  <svg
-                    className="block h-6 w-6 ml-[-25px] text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6 ml-[-70px] text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
+                {/* Icon for open menu */}
+                <svg
+                  className={`block h-6 w-6 ${
+                    isMobileMenuOpen ? "hidden" : "ml-[-52px]"
+                  } text-white`}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+                {/* Icon for close menu */}
+                <svg
+                  className={`block h-6 w-6 ${
+                    isMobileMenuOpen ? "ml-[-52px]" : "hidden"
+                  } text-white`}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
                       d="M4 6h16M4 12h16m-7 6h7"
-                    />
-                  </svg>
-                )}
+                  />
+                </svg>
               </button>
             </div>
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start h-[100%]">

9bb6f05a948d0b25ac268307bc06263bb1dc52ca
Sun-007 <138206213+Umesh-WW@users.noreply.github.com>
2023-07-31 17:27:14 +0530

    corrected for both view ui

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <aside className="bg-gray-200 p-4 md:hidden">
+    <aside className="bg-gray-200 p-4 md:w-64  md:relative md:right-0">
       {/* Add your sidebar content here */}
       Sidebar
     </aside>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,14 @@
-import Footer from '@/components/Layout/Footer';
-import Navbar from '@/components/Layout/Header';
-import Sidebar from '@/components/Layout/Sidebar';
+import Footer from "@/components/Layout/Footer";
+import Navbar from "@/components/Layout/Header";
+import Sidebar from "@/components/Layout/Sidebar";
 import React from "react";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="flex flex-col md:flex-row min-h-screen">
+    <div className="min-h-screen">
       <Navbar />
-      <div className="flex flex-col w-full md:w-auto md:flex-grow md:flex-shrink-0">
+      <div className="flex flex-col md:flex-row">
         <main className="flex-grow p-4">
           <Component {...pageProps} />
         </main>

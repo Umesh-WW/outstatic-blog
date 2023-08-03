127e9d590b0513a99bb856caf913ea4c349ab571
dilip <diliptadha098@gmail.com>
2023-08-03 14:23:17 +0530

    run build

--- src/pages/api/latests/singal.js
+++ src/pages/api/latests/singal.js
@@ -14,6 +14,8 @@ export default async function handler(req, res) {
     res.status(200).json(posts);
   } catch (error) {
     console.error("Error fetching documents:", error);
-    res.status(500).json({ error: "Internal server error" });
+    res
+      .status(500)
+      .json({ error: "Internal server error: something is wrong" });
   }
 }
